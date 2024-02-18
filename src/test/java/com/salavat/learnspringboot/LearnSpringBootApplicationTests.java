bff7cc2fc8833e680020935ed00f98abfac53205
Salavat <salavat.islamov.96@gmail.com>
2024-02-18 17:17:52 +0100

    initial commit

---
+++ src/test/java/com/salavat/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salavat.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
